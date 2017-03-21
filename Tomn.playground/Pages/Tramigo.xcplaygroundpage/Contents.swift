0abc41ceeed1f5c806b18186d9aa76d3247dbea8
tomn94 <tmnstudios@hotmail.fr>
2017-03-21 18:26:57 +0800

    + Tramigo base images

--- Tomn.playground/Pages/Tramigo.xcplaygroundpage/Contents.swift
+++ Tomn.playground/Pages/Tramigo.xcplaygroundpage/Contents.swift
@@ -1,7 +1,19 @@
 //: [Previous](@previous)
 
-import Foundation
+import UIKit
+import PlaygroundSupport
 
-var str = "Hello, playground"
+let view = UIView(frame: CGRect(x: 0, y: 0, width: 450, height: 600))
+PlaygroundPage.current.liveView = view
+
+let backImage = UIImageView(image: #imageLiteral(resourceName: "TramigoBack.jpg"))
+backImage.frame = view.frame
+backImage.contentMode = .scaleAspectFill
+view.addSubview(backImage)
+
+let hand = UIImageView(image: #imageLiteral(resourceName: "TramigoHand.png"))
+let handHeight = view.frame.height / 1.5
+hand.frame = CGRect(x: 0, y: view.frame.height - handHeight, width: handHeight * 2579 / 3487, height: handHeight)
+view.addSubview(hand)
 
 //: [Next](@next)
