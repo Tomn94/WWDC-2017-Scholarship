57b04ed4fc2dc4ae43742fb32bd6c62251ec1f9a
tomn94 <tmnstudios@hotmail.fr>
2017-03-21 18:33:53 +0800

    + Students Union base page + Students Union image resources

--- Tomn.playground/Pages/Students’ Union.xcplaygroundpage/Contents.swift
+++ Tomn.playground/Pages/Students’ Union.xcplaygroundpage/Contents.swift
@@ -1,7 +1,22 @@
 //: [Previous](@previous)
 
-import Foundation
+import UIKit
+import PlaygroundSupport
 
-var str = "Hello, playground"
+let view = UIView(frame: CGRect(x: 0, y: 0, width: 450, height: 600))
+
+/* Configure gradient layer of the background */
+let gradientLayer = CAGradientLayer()
+gradientLayer.frame = view.bounds
+gradientLayer.colors = [#colorLiteral(red: 0.2274509804, green: 0.8431372549, blue: 1, alpha: 1).cgColor, #colorLiteral(red: 0.01176470588, green: 0.6549019608, blue: 1, alpha: 1).cgColor]
+view.layer.addSublayer(gradientLayer)
+
+PlaygroundPage.current.liveView = view
+
+/* Add cloud glyph in center */
+let cloud = UIImageView(image: #imageLiteral(resourceName: "su-cloud.png"))
+cloud.frame = view.frame
+cloud.contentMode = .center
+view.addSubview(cloud)
 
 //: [Next](@next)
