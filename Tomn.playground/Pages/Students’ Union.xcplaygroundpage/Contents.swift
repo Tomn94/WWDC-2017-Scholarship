2ff9912e3989d21e3ed6967776851a7f71f42df8
tomn94 <tmnstudios@hotmail.fr>
2017-03-25 20:49:21 +0800

    + Editable parts

--- Tomn.playground/Pages/Students’ Union.xcplaygroundpage/Contents.swift
+++ Tomn.playground/Pages/Students’ Union.xcplaygroundpage/Contents.swift
@@ -6,6 +6,7 @@
      **Once elected** I further developed the app with the following features.\
      Everything can be managed from a Portal **back-end** I created with 20 services.
  */
+
 //#-hidden-code
 import UIKit
 import PlaygroundSupport
@@ -16,23 +17,24 @@ let mainVC = UIViewController()
 mainVC.view = view
 
 PlaygroundPage.current.liveView = mainVC
-//#-end-hidden-code
 
+//#-end-hidden-code
 /// Resize Features icons
-view.buttonSize = CGSize(width: 42, height: 42)
+view.buttonSize = /*#-editable-code Features icons size*/CGSize(width: 42, height: 42)/*#-end-editable-code*/
 
 /// Wobble angle for icons
-view.wiggleAngle = 0.059
+view.wiggleAngle = /*#-editable-code Maximum left or right angle*/0.059/*#-end-editable-code*/
 
 /// Icons are randomly placed on the view,
-/// defines the maximum number of attempts (watchdog) to find an empty place
-view.maxButtonPlacementAttempt = 142
+/// defines the maximum number of attempts (watchdog) to find an empty place for an icon
+view.maxButtonPlacementAttempt = /*#-editable-code Maximum number of attempts*/142/*#-end-editable-code*/
 
 /// Margin between 2 icons
-view.buttonMargins = UIEdgeInsets(top: -20, left: -20, bottom: -20, right: -20)
+view.buttonMargins = /*#-editable-code Margin between 2 icons*/UIEdgeInsets(top: -20, left: -20, bottom: -20, right: -20)/*#-end-editable-code*/
 
 /// Features of Students’ Union app to display
 let features: [Feature] = [
+//#-editable-code
     Feature(title: "News published by Unions", icon: #imageLiteral(resourceName: "su-newsIcon.png"), screenshot: #imageLiteral(resourceName: "su-newsScreen.png")),
     Feature(title: "Upcoming Events", icon: #imageLiteral(resourceName: "su-eventsIcon.png"), screenshot: #imageLiteral(resourceName: "su-eventsScreen.png")),
     Feature(title: "List of the Unions", icon: #imageLiteral(resourceName: "su-clubsIcon.png"), screenshot: #imageLiteral(resourceName: "su-clubsScreen.png")),
@@ -45,10 +47,13 @@ let features: [Feature] = [
     Feature(title: "Deals & Sponsors", icon: #imageLiteral(resourceName: "su-sponsorsIcon.png"), screenshot: #imageLiteral(resourceName: "su-sponsorsScreen.png")),
     Feature(title: "Buildings Plan & Rooms List", icon: #imageLiteral(resourceName: "su-roomsIcon.png"), screenshot: #imageLiteral(resourceName: "su-roomsScreen.png")),
     Feature(title: "User Profile & Preferences", icon: #imageLiteral(resourceName: "su-userIcon.png"), screenshot: #imageLiteral(resourceName: "su-userScreen.png"))
+//#-end-editable-code
 ]
 view.set(features, in: mainVC)
 
 /// Animate features apparition
+//#-editable-code
 view.showFeatures()
+//#-end-editable-code
 
 //: [Next](@next)
