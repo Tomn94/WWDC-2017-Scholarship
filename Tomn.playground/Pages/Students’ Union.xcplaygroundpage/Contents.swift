434f542d2bfa213371c32c6c47bd396da69c75fb
tomn94 <tmnstudios@hotmail.fr>
2017-03-25 19:19:58 +0800

    ~ Moved most of the code to an external file + Build-In animation + Wiggle animation + Shadow to screenshots + Bold comments

--- Tomn.playground/Pages/Students’ Union.xcplaygroundpage/Contents.swift
+++ Tomn.playground/Pages/Students’ Union.xcplaygroundpage/Contents.swift
@@ -1,117 +1,39 @@
 //: [Previous](@previous)
 /*:
- - callout(A Look at My Projects): Then I created an app for the Students’ Union compaign. One notable feature was Students from my Engineering School had to walk around in the city to find hidden QRcodes, and scan them with the app.\
-     Once elected I further developed the app with the following features.\
-     Everything can be managed from a Portal back-end I created with 20 services.
+ - callout(A Look at My Projects): **Then I created an app for the Students’ Union compaign**.\
+     One notable feature was Students from my Engineering School had to walk around in the city to find hidden QRcodes, and scan them with the app.\
+     \
+     **Once elected** I further developed the app with the following features.\
+     Everything can be managed from a Portal **back-end** I created with 20 services.
  */
 
 import UIKit
 import PlaygroundSupport
 
 //: ### Main View & Background
-let view = UIView(frame: CGRect(x: 0, y: 0, width: 450, height: 600))
-view.tintColor = .white
+let view = FeaturesView(frame: CGRect(x: 0, y: 0, width: 450, height: 600))
 let mainVC = UIViewController()
-mainVC.preferredContentSize = view.frame.size
 mainVC.view = view
-
-/* Configure gradient layer of the background */
-let gradientLayer = CAGradientLayer()
-gradientLayer.frame = view.bounds
-gradientLayer.colors = [#colorLiteral(red: 0.2274509804, green: 0.8431372549, blue: 1, alpha: 1).cgColor, #colorLiteral(red: 0.01176470588, green: 0.6549019608, blue: 1, alpha: 1).cgColor]
-view.layer.addSublayer(gradientLayer)
 
 PlaygroundPage.current.liveView = mainVC
 
-//: ### Title
-let title = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 60))
-title.text = "ESEO Students’ Union"
-title.font = UIFont.boldSystemFont(ofSize: 40)
-title.textColor = .white
-title.numberOfLines = 0
-title.textAlignment = .center
-title.layer.shadowColor   = UIColor.black.cgColor
-title.layer.shadowRadius  = 3
-title.layer.shadowOffset  = CGSize(width: 0, height: 0)
-title.layer.shadowOpacity = 0.4
-view.addSubview(title)
-
-//: ### Add cloud glyph in center
-let cloud = UIImageView(image: #imageLiteral(resourceName: "su-cloud.png"))
-cloud.frame = view.frame
-cloud.center.y += title.frame.height * 2
-cloud.contentMode = .center
-view.addSubview(cloud)
-
 let features: [Feature] = [
     Feature(title: "News published by Unions", icon: #imageLiteral(resourceName: "su-newsIcon.png"), screenshot: #imageLiteral(resourceName: "su-newsScreen.png")),
-    
     Feature(title: "Upcoming Events", icon: #imageLiteral(resourceName: "su-eventsIcon.png"), screenshot: #imageLiteral(resourceName: "su-eventsScreen.png")),
-    
     Feature(title: "List of the Unions", icon: #imageLiteral(resourceName: "su-clubsIcon.png"), screenshot: #imageLiteral(resourceName: "su-clubsScreen.png")),
-    
     Feature(title: "Student Families", icon: #imageLiteral(resourceName: "su-familiesIcon.png"), screenshot: #imageLiteral(resourceName: "su-familiesScreen.png")),
-    
     Feature(title: "Shared Documents", icon: #imageLiteral(resourceName: "su-docsIcon.png"), screenshot: #imageLiteral(resourceName: "su-docsScreen.png")),
-    
     Feature(title: "Meal Ordering System,\nincludes paying with iOS", icon: #imageLiteral(resourceName: "su-orderIcon.png"), screenshot: #imageLiteral(resourceName: "su-orderScreen.png")),
-    
     Feature(title: "Push Notifications for Events and Deliveries", icon: #imageLiteral(resourceName: "su-pushIcon.png"), screenshot: #imageLiteral(resourceName: "su-pushScreen.png")),
-    
     Feature(title: "Events & Tickets Ordering,\nincludes paying with iOS", icon: #imageLiteral(resourceName: "su-ticketsIcon.png"), screenshot: #imageLiteral(resourceName: "su-ticketsScreen.png")),
-    
     Feature(title: "iMessage Stickers", icon: #imageLiteral(resourceName: "su-stickersIcon.png"), screenshot: #imageLiteral(resourceName: "su-stickersScreen.png")),
-    
     Feature(title: "Deals & Sponsors", icon: #imageLiteral(resourceName: "su-sponsorsIcon.png"), screenshot: #imageLiteral(resourceName: "su-sponsorsScreen.png")),
-    
     Feature(title: "Buildings Plan & Rooms List", icon: #imageLiteral(resourceName: "su-roomsIcon.png"), screenshot: #imageLiteral(resourceName: "su-roomsScreen.png")),
-    
     Feature(title: "User Profile & Preferences", icon: #imageLiteral(resourceName: "su-userIcon.png"), screenshot: #imageLiteral(resourceName: "su-userScreen.png"))
 ]
 
-let center = view.center
-let cloudSize = CGSize(width: 300, height: 200)
-let titleHeight = title.frame.height
-let xUpperBound = UInt32(cloudSize.width * 1.2)
-let yUpperBound = UInt32(center.y - (titleHeight * 1.2))
-let angleBound  = UInt32(20)    // degrees
-let margins = UIEdgeInsets(top: -20, left: -20, bottom: -20, right: -20)
+view.setFeatures(features, in: mainVC)
 
-var buttonsFrames = [CGRect]()
-
-extension CGRect {
-    
-    func intersects(_ rects: [CGRect]) -> Bool {
-        
-        for rect in rects {
-            if self.intersects(rect) {
-                return true
-            }
-        }
-        return false
-        
-    }
-    
-}
-
-for feature in features {
-    
-    var newButtonFrame = CGRect.zero
-    repeat {
-        let x = center.x - cloudSize.width / 1.5 + CGFloat(arc4random_uniform(xUpperBound))
-        let y = titleHeight + CGFloat(arc4random_uniform(yUpperBound))
-        newButtonFrame = CGRect(x: x, y: y, width: 42, height: 42)
-    } while newButtonFrame.intersects(buttonsFrames)
-    
-    let button = PopButton(frame: newButtonFrame)
-    button.feature = feature
-    button.mainController = mainVC
-    button.transform = CGAffineTransform(rotationAngle: (-CGFloat(angleBound) / 2 + CGFloat(arc4random_uniform(angleBound))) * CGFloat.pi / 180)
-
-    view.addSubview(button)
-    buttonsFrames.append(UIEdgeInsetsInsetRect(button.frame, margins))
-}
-
-
+view.showFeatures()
 
 //: [Next](@next)
--- Tomn.playground/Sources/StudentsUnion.swift
+++ Tomn.playground/Sources/StudentsUnion.swift
@@ -1,5 +1,20 @@
 import UIKit
 
+extension CGRect {
+    /// Checks whether a frame intersects with others
+    ///
+    /// - Parameter rects: Frames to compare with
+    /// - Returns: If the frame intersects with any other
+    func intersects(_ rects: [CGRect]) -> Bool {
+        for rect in rects {
+            if self.intersects(rect) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
 public struct Feature {
     public let title: String
     public let icon: UIImage
@@ -32,6 +47,9 @@ public class PopoverController: UIViewController, UIPopoverPresentationControlle
         
         let screenshot = UIImageView(image: feature.screenshot)
         screenshot.contentMode = .scaleAspectFit
+        screenshot.layer.shadowOpacity = 0.3
+        screenshot.layer.shadowOffset = CGSize.zero
+        screenshot.layer.shadowRadius = 5
         
         let stackView = UIStackView(arrangedSubviews: [description, screenshot])
         stackView.axis = .vertical
@@ -83,4 +101,153 @@ public class PopButton: UIButton {
         mainController.present(popover, animated: true)
     }
     
+}
+
+public class FeaturesView: UIView {
+    
+    /* Configure gradient layer of the background */
+    let gradientLayer = CAGradientLayer()
+    
+    let title = UILabel()
+    
+    let cloud = UIImageView()
+    
+    var features = [Feature]()
+    
+    var viewController: UIViewController?
+    
+    var buttons = [UIButton]()
+    
+    var buttonsFrames = [CGRect]()
+    
+    var areFeaturesHidden = true
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.tintColor = .white
+        self.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        gradientLayer.colors = [#colorLiteral(red: 0.2274509804, green: 0.8431372549, blue: 1, alpha: 1).cgColor, #colorLiteral(red: 0.01176470588, green: 0.6549019608, blue: 1, alpha: 1).cgColor]
+        self.layer.addSublayer(gradientLayer)
+        
+        
+        //: ### Title
+        title.frame = CGRect(x: 0, y: 0, width: frame.width, height: 60)
+        title.text = "ESEO Students’ Union"
+        title.font = UIFont.boldSystemFont(ofSize: 40)
+        title.textColor = .white
+        title.numberOfLines = 0
+        title.textAlignment = .center
+        title.layer.shadowColor   = UIColor.black.cgColor
+        title.layer.shadowRadius  = 3
+        title.layer.shadowOffset  = CGSize(width: 0, height: 0)
+        title.layer.shadowOpacity = 0.4
+        title.autoresizingMask    = .flexibleWidth
+        self.addSubview(title)
+        
+        //: ### Add cloud glyph in center
+        cloud.image = #imageLiteral(resourceName: "su-cloud.png")
+        cloud.frame = frame
+        cloud.center.y += title.frame.height * 2
+        cloud.contentMode = .center
+        cloud.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.addSubview(cloud)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        gradientLayer.frame = self.bounds
+        
+        computeRects()
+        if !areFeaturesHidden {
+            for (index, button) in buttons.enumerated() {
+                button.frame = buttonsFrames[index]
+            }
+        }
+    }
+    
+    public func setFeatures(_ features: [Feature], in viewController: UIViewController) {
+        
+        self.features = features
+        self.viewController = viewController
+        
+        buttons.forEach { $0.removeFromSuperview() }
+        buttons = []
+        computeRects()
+        
+        for (index, feature) in features.enumerated() {
+            
+            let button = PopButton(frame: CGRect(origin: cloud.center, size: buttonsFrames[index].size))
+            button.feature = feature
+            button.mainController = viewController
+            
+            buttons.append(button)
+            self.addSubview(button)
+        }
+        
+        self.bringSubview(toFront: cloud)
+    }
+    
+    public func computeRects() {
+        
+        buttonsFrames = [CGRect]()
+        var buttonsFramesWithInset = [CGRect]()
+        
+        let cloudSize = CGSize(width: 300, height: 200)
+        let titleHeight = title.frame.height
+        let xUpperBound = UInt32(cloudSize.width * 1.2)
+        let yUpperBound = UInt32(center.y - (titleHeight * 1.2))
+        let margins = UIEdgeInsets(top: -20, left: -20, bottom: -20, right: -20)
+        
+        for _ in features {
+            
+            var newButtonFrame = CGRect.zero
+            var attempts = 0
+            repeat {
+                let x = center.x - cloudSize.width / 1.5 + CGFloat(arc4random_uniform(xUpperBound))
+                let y = titleHeight + CGFloat(arc4random_uniform(yUpperBound))
+                
+                newButtonFrame = CGRect(x: x, y: y, width: 42, height: 42)
+                attempts += 1
+                
+            } while newButtonFrame.intersects(buttonsFramesWithInset) && attempts < 42
+            
+            buttonsFramesWithInset.append(UIEdgeInsetsInsetRect(newButtonFrame, margins))
+            buttonsFrames.append(newButtonFrame)
+        }
+    }
+    
+    public func showFeatures() {
+        
+        areFeaturesHidden = false
+        
+        for (index, button) in self.buttons.enumerated() {
+            
+            button.frame = CGRect(origin: cloud.center, size: buttonsFrames[index].size)
+            
+            UIView.animate(withDuration: 0.6, delay: 0.2 + TimeInterval(arc4random_uniform(10)) / 20, options: [.curveEaseIn], animations: {
+                button.frame = self.buttonsFrames[index]
+            }, completion: { _ in
+                
+                let animation = CAKeyframeAnimation(keyPath: "transform")
+                animation.autoreverses = true
+                animation.duration = 0.13
+                animation.repeatCount = Float.greatestFiniteMagnitude
+                
+                let wiggleAngle: CGFloat = 0.059
+                let left  = NSValue(caTransform3D: CATransform3DMakeRotation(wiggleAngle, 0, 0, 1))
+                let right = NSValue(caTransform3D: CATransform3DMakeRotation(-wiggleAngle, 0, 0, 1))
+                animation.values = [left, right]
+                
+                button.layer.add(animation, forKey:"")
+            })
+        }
+    }
+
 }
