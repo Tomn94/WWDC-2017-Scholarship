70dcd51338485d73fc2a1222e601fd5f8bb81017
tomn94 <tmnstudios@hotmail.fr>
2017-03-25 16:11:23 +0800

    ~ Rotation & autolayout fixes for the background layer & avatar ~ Use first & last color twice so as they can be seen

--- Tomn.playground/Sources/Me.swift
+++ Tomn.playground/Sources/Me.swift
@@ -58,10 +58,23 @@ public class RotatingResponder: SCNView {
         }
     }
     
+    /// Fixes scene not resizing when rotating screen
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        runSpinningHintAnimation()
+    }
+    
 }
 
 /// View with a gradient background, colors rotating
 public class AnimatedGradientView: UIView {
+    
+    /// Animated gradient layer of the view
+    let gradientLayer = CAGradientLayer()
+    
+    /// Background color rotation animation speed
+    var speed: TimeInterval = 20
     
     /// Configure gradient layer
     ///
@@ -72,33 +85,59 @@ public class AnimatedGradientView: UIView {
     public init(frame: CGRect, colors: [UIColor], speed: TimeInterval) {
         super.init(frame: frame)
         
+        self.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.speed = speed
+        
         /* Configure gradient layer of the background */
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = self.bounds
         gradientLayer.anchorPoint = CGPoint.zero
-        gradientLayer.frame.size.width  *= 7
-        gradientLayer.frame.size.height *= 5
         gradientLayer.startPoint = CGPoint.zero
         gradientLayer.endPoint = CGPoint(x: 1, y: 1)
-        gradientLayer.colors = colors.map { $0.cgColor }
+        var adjustedColors = colors.map { $0.cgColor }
+        if let firstColor = colors.first {
+            adjustedColors.insert(firstColor.cgColor, at: 0)
+        }
+        if let lastColor = colors.last {
+            adjustedColors.append(lastColor.cgColor)
+        }
+        gradientLayer.colors = adjustedColors
+        
+        configureLayer()
+        
         self.layer.addSublayer(gradientLayer)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /// Ajust background layer to screen rotation
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        configureLayer()
+    }
+    
+    /// Set up gradient layer
+    func configureLayer() {
+        
+        gradientLayer.frame = self.bounds
+        gradientLayer.frame.size.width  *= 7
+        gradientLayer.frame.size.height *= 5
         
         /* Animate gradient */
+        self.layer.removeAllAnimations()
+        
         let gradientSize = gradientLayer.frame.size
         let backgroundSize = frame.size
         let animation = CABasicAnimation(keyPath: "position")
         animation.fromValue = CGPoint.zero
-        animation.toValue = CGPoint(x: -gradientSize.width  + backgroundSize.width,
-                                    y: -gradientSize.height + backgroundSize.height)
+        animation.toValue = CGPoint(x: -gradientSize.width  + backgroundSize.width * 2,
+                                    y: -gradientSize.height + backgroundSize.height * 2)
         animation.repeatCount = Float.greatestFiniteMagnitude
         animation.duration = speed
         animation.autoreverses = true
         animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
         self.layer.add(animation, forKey: "animateGradient")
-    }
-    
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
 }
