78be9c5fdce90b2b88c4e5b99172ecb3eca067b6
tomn94 <tmnstudios@hotmail.fr>
2017-03-26 17:35:57 +0800

    + Comments

--- Tomn.playground/Sources/Tilt.swift
+++ Tomn.playground/Sources/Tilt.swift
@@ -2,10 +2,18 @@ import UIKit
 
 public extension UIView {
     
+    /// Add parallax animation to an UIView when moving device
+    ///
+    /// - Parameters:
+    ///   - tiltFact: Reaction factor to tilting device
+    ///   - shadowFact: Shadow move when tilting
+    ///   - panFact: Distortion reaction to titling
     public func setUpParallax(tiltFact: CGFloat = 0.5, shadowFact: CGFloat = 15, panFact: CGFloat = 20) {
         
+        /* Remove any previous effect */
         self.motionEffects.removeAll()
         
+        /* Distortion */
         let tiltX = UIInterpolatingMotionEffect(keyPath: "layer.transform.rotation.x", type: .tiltAlongHorizontalAxis)
         tiltX.minimumRelativeValue = -tiltFact
         tiltX.maximumRelativeValue = tiltFact
@@ -14,6 +22,7 @@ public extension UIView {
         tiltY.minimumRelativeValue = -tiltFact
         tiltY.maximumRelativeValue = tiltFact
         
+        /* Shadow offset */
         let shadowX = UIInterpolatingMotionEffect(keyPath: "layer.shadowOffset.width", type: .tiltAlongHorizontalAxis)
         shadowX.minimumRelativeValue = -shadowFact
         shadowX.maximumRelativeValue = shadowFact
@@ -22,6 +31,7 @@ public extension UIView {
         shadowY.minimumRelativeValue = -shadowFact
         shadowY.maximumRelativeValue = shadowFact
         
+        /* Move */
         let panX = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
         panX.minimumRelativeValue = -panFact
         panX.maximumRelativeValue = panFact
@@ -30,10 +40,12 @@ public extension UIView {
         panY.minimumRelativeValue = -panFact
         panY.maximumRelativeValue = panFact
         
+        /* Light Glare effect */
         let alpha = UIInterpolatingMotionEffect(keyPath: "alpha", type: .tiltAlongVerticalAxis)
         alpha.minimumRelativeValue = -0.9
         alpha.maximumRelativeValue = 1
         
+        /* Add them all! */
         let motionGroup = UIMotionEffectGroup()
         motionGroup.motionEffects = [tiltX, tiltY, shadowX, shadowY, panX, panY, alpha]
         self.addMotionEffect(motionGroup)
