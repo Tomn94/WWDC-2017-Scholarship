f342cfb6a968b4324577aa35131608d842fd47a0
tomn94 <tmnstudios@hotmail.fr>
2017-03-30 21:01:29 +0800

    + Editable & hidden code

--- Tomn.playgroundbook/Contents/Chapters/Chapter3.playgroundchapter/Pages/WWDC.playgroundpage/Contents.swift
+++ Tomn.playgroundbook/Contents/Chapters/Chapter3.playgroundchapter/Pages/WWDC.playgroundpage/Contents.swift
@@ -1,10 +1,16 @@
+//#-hidden-code
 import SpriteKit
 import PlaygroundSupport
 
 let mainView = SKView(frame: CGRect(x: 0, y: 0, width: 430, height: 600))
-
 let scene = WWDCScene(size: mainView.frame.size)
-scene.participants = 42
+
+// Change how many people participate in WWDC
+//#-hidden-code
+scene.participants = /*#-editable-code Change how many people participate in WWDC*/42/*#-end-editable-code*/
+//#-end-hidden-code
+
 mainView.presentScene(scene)
 
 PlaygroundPage.current.liveView = mainView
+//#-end-hidden-code
