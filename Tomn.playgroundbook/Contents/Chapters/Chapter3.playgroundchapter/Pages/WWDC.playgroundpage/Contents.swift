30f85001166048a6196362035fc81bed8e8e6b2a
tomn94 <tmnstudios@hotmail.fr>
2017-03-30 19:14:50 +0800

    + Base scene and person

--- Tomn.playgroundbook/Contents/Chapters/Chapter3.playgroundchapter/Pages/WWDC.playgroundpage/Contents.swift
+++ Tomn.playgroundbook/Contents/Chapters/Chapter3.playgroundchapter/Pages/WWDC.playgroundpage/Contents.swift
@@ -1,7 +1,51 @@
-//: [Previous](@previous)
+import SpriteKit
+import PlaygroundSupport
 
-import Foundation
+class Person: SKSpriteNode {
+    
+    static let appearances = []
+    
+    convenience init() {
+        self.init(imageNamed: "can")
+        
+        let body = SKPhysicsBody(circleOfRadius: 10)
+        body.affectedByGravity = false
+        self.physicsBody = body
+    }
+    
+}
 
-var str = "Hello, playground"
+class WWDCScene: SKScene {
+    
+    let title = SKLabelNode(text: "Rejoice for WWDC17!")
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = #colorLiteral(red: 0.9725490196, green: 0.9725490196, blue: 0.9725490196, alpha: 1)
+        
+        scaleMode = .resizeFill
+        
+        title.position = self.view?.center ?? .zero
+        title.fontName = UIFont.boldSystemFont(ofSize: 22).fontName
+        title.fontColor = #colorLiteral(red: 0.2352941176, green: 0.2352941176, blue: 0.2352941176, alpha: 1)
+        addChild(title)
+        
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        let person = Person()
+        person.position = view.center
+        addChild(person)
+    }
+    
+    override func didChangeSize(_ oldSize: CGSize) {
+        super.didChangeSize(oldSize)
+        
+        title.position = self.view?.center ?? .zero
+    }
+}
 
-//: [Next](@next)
+let mainView = SKView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
+
+let scene = WWDCScene(size: mainView.frame.size)
+mainView.presentScene(scene)
+
+PlaygroundPage.current.liveView = mainView
