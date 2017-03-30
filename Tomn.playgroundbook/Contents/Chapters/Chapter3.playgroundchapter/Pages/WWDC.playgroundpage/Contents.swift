8dd43a4809b86d8c40aee3f6e498fd3ebaa9e2ac
tomn94 <tmnstudios@hotmail.fr>
2017-03-30 20:41:04 +0800

    ~ View adjustments + 30 persons ~ Velocity and scale tweaks

--- Tomn.playgroundbook/Contents/Chapters/Chapter3.playgroundchapter/Pages/WWDC.playgroundpage/Contents.swift
+++ Tomn.playgroundbook/Contents/Chapters/Chapter3.playgroundchapter/Pages/WWDC.playgroundpage/Contents.swift
@@ -6,33 +6,32 @@ class Person: SKSpriteNode {
     static let nbrAppearances: UInt32 = 10
     
     convenience init() {
+        
         let randomIndex = 1 + Int(arc4random_uniform(Person.nbrAppearances))
         
         self.init(imageNamed: "person\(randomIndex)")
         
         name = "person"
-        
-        setScale(0.7)
+        setScale(0.3)
         
         let body = SKPhysicsBody(circleOfRadius: 30)
         body.allowsRotation = false
         body.affectedByGravity = false
         body.linearDamping = 0   // friction
         body.restitution = 1     // don't lose velocity when colliding
-        let randomDest = Int(arc4random_uniform(100))
+        let randomDest = Int(arc4random_uniform(50))
         let signX: CGFloat = randomDest & 1 == 0 ? -1 : 1
         let signY: CGFloat = (randomDest & 2) >> 1 == 0 ? -1 : 1
         /// Initial speed of the person
         let velocityX = CGFloat(randomDest)
-        let velocityY = CGFloat(arc4random_uniform(100))
+        let velocityY = CGFloat(arc4random_uniform(50))
         body.velocity = CGVector(dx: signX * velocityX,
                                  dy: signY * velocityY)
         body.contactTestBitMask = 1  // handle contacts
         self.physicsBody = body
         
-        let angle = atan2(body.velocity.dy, body.velocity.dx)
-        
-        zRotation = angle
+        // Walk orientation
+        zRotation = atan2(body.velocity.dy, body.velocity.dx)
     }
     
 }
@@ -57,13 +56,13 @@ class WWDCScene: SKScene {
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         physicsWorld.contactDelegate = self
         
-        let person = Person()
-        person.position = view.center
-        addChild(person)
-        
-        let person2 = Person()
-        person2.position = CGPoint(x: 100, y: 100)
-        addChild(person2)
+        for _ in 0..<30 {
+            let posX = Int(arc4random_uniform(UInt32(size.width)))
+            let posY = Int(arc4random_uniform(UInt32(size.height)))
+            let person = Person()
+            person.position = CGPoint(x: posX, y: posY)
+            addChild(person)
+        }
     }
     
     override func didChangeSize(_ oldSize: CGSize) {
@@ -124,12 +123,12 @@ extension WWDCScene: SKPhysicsContactDelegate {
         let velocity = person.physicsBody?.velocity ?? .zero
         let angle = atan2(velocity.dy, velocity.dx)
         
-        person.run(.rotate(toAngle: angle, duration: 0.25))
+        person.run(.rotate(toAngle: angle, duration: 0.42))
     }
     
 }
 
-let mainView = SKView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
+let mainView = SKView(frame: CGRect(x: 0, y: 0, width: 430, height: 600))
 
 let scene = WWDCScene(size: mainView.frame.size)
 mainView.presentScene(scene)
